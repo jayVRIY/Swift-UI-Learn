140d0b74da8a4a597fc0e9e2e3a0632a48a7182d
jayVRIY <yuanjie1224@gmail.com>
2022-09-14 19:34:15 +0800

    Initial Commit

---
+++ SwiftUI Learn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Learn
+//
+//  Created by Jay on 2022/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Learn/SwiftUI_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearnApp.swift
+//  SwiftUI Learn
+//
+//  Created by Jay on 2022/9/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
